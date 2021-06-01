ac76afafe6958d17b7745563554af5858555dc18
wellingtondsf <wellington@bridge.ufsc.br>
2021-05-31 23:50:31 -0300

    subscription on stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,41 @@
-import styles from './styles.module.scss'
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string;
+  priceId: string;
 }
 
-export const SubscribeButton = ({priceId} : SubscribeButtonProps) => {
-    return (
-        <button type='button' className={styles.subscribeButton}>
-            Subscribe now
-        </button>
-    )
-}
+export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId});
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
+};
