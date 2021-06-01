d76b810fade89bbc73d3851fe66613ddc5c63346
wellingtondsf <wellington@bridge.ufsc.br>
2021-05-31 21:27:48 -0300

    setting up faunadb and saving user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { fauna } from '../../../services/fauna' 
+import { query as q } from 'faunadb' 
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -9,4 +12,39 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+
+  callbacks: {
+    async signIn(user){
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
